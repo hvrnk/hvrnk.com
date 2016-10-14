69343f148c2e853c45f4c5aa104168501f793d9b
jay havranek <jay@engagereal.com>
2016-10-13 18:44:56 -0600

    create inline style objects for header and nav bar

---
+++ src/assets/styles/header.js
@@ -0,0 +1,12 @@
+export const container = {
+  height: '10%',
+  width: '100%',
+  backgroundColor: '#343434'
+};
+
+export const hvrnk = {
+  color: 'orange',
+  margin: '0',
+  textAlign: 'center',
+  fontSize: '38'
+};
---
+++ src/assets/styles/navBar.js
@@ -0,0 +1,15 @@
+export const container = {
+  border: 'gray solid 2px',
+  borderTop: 'none',
+  backgroundColor: 'green',
+  textAlign: 'center'
+};
+
+export const navList = {
+  listStyle: 'none',
+  margin: '0',
+  padding: '0',
+  display: 'inline-block',
+  textColor: 'orange',
+  textDecoration: 'none'
+};
